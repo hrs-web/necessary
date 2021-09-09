923d854dd7c4cdaa8db004a1f0a0f57912680db2
hrs-web <2233547339@qq.com>
2021-09-09 09:53:54 +0800

    springboot整合thymeleaf

--- springboot-user/src/main/java/cn/hrsweb/user/controller/UserController.java
+++ springboot-user/src/main/java/cn/hrsweb/user/controller/UserController.java
@@ -4,6 +4,7 @@ import cn.hrsweb.user.pojo.User;
 import cn.hrsweb.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +25,12 @@ public class UserController {
     }
 
     @GetMapping("all")
-    @ResponseBody
-    public List<User> queryUserAll(){
-        return userService.queryUserAll();
+    public String queryUserAll(Model model){
+        // 查询用户
+        List<User> users = userService.queryUserAll();
+        // 放入模型
+        model.addAttribute("users",users);
+        // 返回模板名称(classpath:/templates/目录下html文件名)
+        return "users";
     }
 }
