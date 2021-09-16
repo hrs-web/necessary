5120f26ebfa0c73b59bba2110e57eed008213eb4
hrs-web <2233547339@qq.com>
2021-09-16 23:11:54 +0800

    Ribbon

--- user-service-consumer/src/main/java/cn/hrsweb/consumer/UserServiceConsumerApplication.java
+++ user-service-consumer/src/main/java/cn/hrsweb/consumer/UserServiceConsumerApplication.java
@@ -3,6 +3,7 @@ package cn.hrsweb.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient  //开启Eureka的客户端
 public class UserServiceConsumerApplication {
     @Bean
+    @LoadBalanced  // 开启ribbon的负载均衡
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- user-service-consumer/src/main/java/cn/hrsweb/consumer/controller/UserController.java
+++ user-service-consumer/src/main/java/cn/hrsweb/consumer/controller/UserController.java
@@ -6,10 +6,12 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,6 +23,7 @@ public class UserController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    // eureka演示
     @GetMapping
     public User queryById(@PathParam("id")Long id){
         // 通过服务的id获取服务实例的集合
@@ -29,5 +32,21 @@ public class UserController {
         System.out.println(instance.getHost());
         System.out.println(instance.getPort());
         return restTemplate.getForObject("http://"+instance.getHost()+":"+instance.getPort()+"/provider/"+id,User.class);
+    }
+
+
+    // Ribbon演示
+    @GetMapping("user")
+    public List<User> queryByUserIds(@RequestParam(value = "ids",required=false)List<Long> ids){
+        List<User> users = new ArrayList<>();
+        ids.forEach(id -> {
+            users.add(restTemplate.getForObject("http://PROVIDER-SERVER/provider/"+id,User.class));
+            try {
+                Thread.sleep(500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        });
+        return users;
     }
 }
